c3eae8cd223dcd8dec890ffb60a24f6dd1aafbb7
mathter <mathter7@gmail.com>
2025-02-17 20:49:18 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,54 @@
 use qrcode::QrCode;
-// use qrcode::render::svg;
 use qrcode::render::unicode;
-use std::env;
+use clap::Parser;
+
+/// Print QR code to the terminal
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Text to encode in the QR code
+    #[arg(short, long)]
+    text: String,
+
+    /// Use Sixel graphics to render the QR code
+    #[arg(short='S', long, action)]
+    sixel: bool,
+
+    /// Scale factor for the QR code. (Only used with Sixel graphics)
+    #[arg(short, long, default_value_t = 1)]
+    scale: usize,
+}
 
 fn main(){
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        println!("Usage: {} <message>", args[0]);
-        return;
+    let message = &args.text;
+
+    let scale = args.scale;
+
+    let is_sixel = args.sixel;
+    if is_sixel {
+        render_qr_sixel(message, scale);
+    } else {
+        render_qr_unicode(message);
     }
 
-    // 1個目の引数を取得(QRコードにする文字列)
-    let message = &args[1];
+}
 
-    // 2個目の引数を取得(拡大倍率)
-    // TODO 0は排除
-    let scale = if args.len() > 2 {
-        args[2].parse().unwrap()
-    } else {
-        1
-    };
-
-
-    // Encode some data into bits.
+fn render_qr_unicode(message: &str) {
     let code = QrCode::new(message).unwrap();
-
-    // You can also render it into a string.
     let string = code.render()
-        // .light_color(' ')
-        // .dark_color('*')
-        // .dark_color(svg::Color("#800000"))
-        // .light_color(svg::Color("#ffff80"))
         .dark_color(unicode::Dense1x2::Light)
         .light_color(unicode::Dense1x2::Dark)
         .build();
     println!("{}", string);
+}
+
+
+fn render_qr_sixel(message: &str, scale: usize) {
+
+    // Encode some data into bits.
+    let code = QrCode::new(message).unwrap();
 
     println!("\n");
 
@@ -45,8 +58,6 @@ fn main(){
     let lines: Vec<&str> = string2.split("\n").collect();
     let rows = lines.len();
     let cols = lines[0].len();
-    println!("rows:{}", lines.len());
-    println!("cols:{}", lines[0].len());
 
     let scaled_row = rows * scale;
     let scaled_col = cols * scale;
@@ -69,23 +80,13 @@ fn main(){
     // Sixel エスケープシーケンス開始
     print!("\x1bPq");
 
-    // 文字列"}G}"を出力
-    // print!("}}G}}");
-    let mut row_final = 0;
-
     // 0からrows-1までループ
     for i in 0 ..((scaled_row + 1)/6) {
-        // lines[i]を出力
-        // print!("{}", lines[i]);
-
         for j in 0 ..scaled_col {
             let mut value: u8 = 0;
 
             let mut row = i * 6;
             for k in 0 ..6 {
-                // if lines[row].chars().nth(j).unwrap() == '0' {
-                //     value |= 1 << k;
-                // }
                 if qr_obj[row][j] == '0' {
                     value |= 1 << k;
                 }
@@ -96,36 +97,15 @@ fn main(){
                 }
             }
 
-            row_final = row;
-
             let c = (value + 63) as char;
             print!("{}", c);
-    
-            // // 文字列"}"を出力
-            // // 0からrows-1までループ
-            // for j in 0 ..rows {
-            //     // lines[j][i]を出力
-            //     print!("{}", lines[j].chars().nth(i).unwrap());
-            // }
-            
         }
 
         print!("-");
     }
 
-    // 0からcols-1までループ
-
-    
-
     // Sixel エスケープシーケンス終了
     print!("\x1b\\");
 
     println!("\n");
-
-    println!("row_final:{}", row_final);
-
 }
-
-
-// black:   \e[40m
-//white:   \e[47m
